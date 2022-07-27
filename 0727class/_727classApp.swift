dc94a19cc306d925556e0c5ff2bea4e3f62c2fff
Wongi <hpurs95@gmail.com>
2022-07-27 23:48:00 +0900

    Initial Commit

---
+++ 0727class/_727classApp.swift
@@ -0,0 +1,17 @@
+//
+//  _727classApp.swift
+//  0727class
+//
+//  Created by MacBook Air on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct _727classApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
